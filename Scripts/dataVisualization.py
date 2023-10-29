5c44e40083b626d9377166dc06ff042d0392ab46
Jacob Quintero <jquintero9966@tamu.edu>
2023-10-29 11:57:01 -0500

    Data Visualization Script Added

--- Scripts/dataVisualization.py
+++ Scripts/dataVisualization.py
@@ -17,12 +17,24 @@ engine = create_engine(connection_string)
 query = "SELECT district_total, total_title1_enrolled FROM school_summary"
 df = pd.read_sql(query, engine)
 
+# Remove data points where the district population is less than 100
+df = df[df['district_total'] >= 100]
+
+# Constrain 'total_title1_enrolled' to be between 1 (to exclude 0) and 100,000
+df = df[(df['total_title1_enrolled'] > 0) & (df['total_title1_enrolled'] <= 100000)]
+
+# Remove the top 10 extreme values for 'district_total' and 'total_title1_enrolled'
+sorted_district = df['district_total'].sort_values(ascending=False).iloc[10:]
+sorted_title1 = df['total_title1_enrolled'].sort_values(ascending=False).iloc[10:]
+
+df_filtered = df[df['district_total'].isin(sorted_district) & df['total_title1_enrolled'].isin(sorted_title1)]
+
 # Visualizing the data
 plt.figure(figsize=(10,6))
-plt.scatter(df['district_total'], df['total_title1_enrolled'], alpha=0.6, edgecolors="w", linewidth=0.5)
+plt.scatter(df_filtered['total_title1_enrolled'], df_filtered['district_total'], alpha=0.6, edgecolors="w", linewidth=0.5)
 plt.xlabel('District Total')
 plt.ylabel('Students in Poverty')
-plt.title('Impoverished Students vs District Density')
+plt.title('Impoverished Students vs District Density (Filtered Data)')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 
 # Display the plot
